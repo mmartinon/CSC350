e8f6dfe5b9adb89dfeea6ab17ae766c1d51b7814
atoomer <aarondtoomer@gmail.com>
2025-02-18 16:50:23 -0500

    Added comments and Overrides

--- java/search/SubNavProblem.java
+++ java/search/SubNavProblem.java
@@ -8,6 +8,12 @@ public class SubNavProblem extends Problem {
     private Station startStation;
     private Station goalStation;
 
+    /**
+     * Constructor to initialize the subway navigation problem.
+     * @param map The subway map containing all stations and links.
+     * @param startStation The starting station in the problem.
+     * @param goalStation The goal station in the problem.
+     */
     public SubNavProblem(SubwayMap map, Station startStation, Station goalStation) {
         super(new State(startStation.name), new State(goalStation.name));
         this.map = map;
@@ -15,19 +21,37 @@ public class SubNavProblem extends Problem {
         this.goalStation = goalStation;
     }
 
-    public State initialState() {
+    /**
+     * Returns the initial state of the problem.
+     * @return The initial state.
+     */
+    @Override
+    public State getInitial() {
         return new State(startStation.name);
     }
-    public State goalState() {
+
+    /**
+     * Returns the goal state of the problem.
+     * @return The goal state.
+     */
+    public State getGoal() {
         return new State(goalStation.name);
     }
 
+    /**
+     * Generates the successors of a given state.
+     * @param state The current state.
+     * @return A list of tuples containing actions and resulting states.
+     */
+    @Override
     public ArrayList<Tuple> successor(State state) {
         ArrayList<Tuple> successors = new ArrayList<>();
         Station currentStation = map.getStationByName(state.getName());
 
+        // Get adjacent stations to the current station
         ArrayList<Station> adjacentStations = map.adjacentStations(currentStation);
 
+        // Iterate through adjacent stations and create successor states
         for (Station adjacentStation : adjacentStations) {
             ArrayList<Link> links = map.getLinksBetween(currentStation, adjacentStation);
             for (Link link : links) {
@@ -40,11 +64,22 @@ public class SubNavProblem extends Problem {
         return successors;
     }
 
+
+    /**
+     * Calculates the path cost from the current state to the next state.
+     * @param pathCost The current path cost.
+     * @param state The current state.
+     * @param action The action taken.
+     * @param nextState The resulting state.
+     * @return The updated path cost.
+     */
+    @Override
     public double pathCost(double pathCost, State state, Action action, State nextState) {
 
         Station currentStation = map.getStationByName(state.getName());
         Station nextStation = map.getStationByName(nextState.getName());
 
+        // Get links between the current station and the next station
         ArrayList<Link> links = map.getLinksBetween(currentStation, nextStation);
         for (Link link : links) {
             if (link.line.equals(action.getName())) {
